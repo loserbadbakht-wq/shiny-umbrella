66aec5871589dfea6965190c388c85ad3ae5fdea
loserbadbakht-wq <loserbadbakht@gmail.com>
2026-08-27 22:53:21 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
         await ctx.reply(message, { 
             parse_mode: 'Markdown',
-            disable_web_page_preview: true
-        });
-    }
+            disable_web_page_preview: tru
           }
